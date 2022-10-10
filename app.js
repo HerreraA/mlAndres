f5baa403775314859fdd0dc590f2d4b1be8aad6c
HerreraA <herrera.andresg@gmail.com>
2022-10-09 22:52:38 -0500

    tercer commit

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 const express = require("express");
 const path = require ("path");
+const port =  process.env.PORT || 3001;
 
 const app = express()
 const publicPath= path.resolve(__dirname, "./public")
 app.use(express.static(publicPath))
 
-app.listen(3000, ()=> {
-    console.log("Servidor corriendo en el puerto 3000, link puerto: http://localhost:3000")
-})
 
 app.get("/", (req, res)=>{
     res.sendFile(path.resolve(__dirname, "./views/home.html"))
 })
 
-app.get('/register', (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/register.html"));
+app.get('/register.html', (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/register.html"));
     
 })
 
-app.get('/login', (req, res) => {
-    let htmlPath = path.resolve(__dirname, './views/login.html');
+app.get('/login.html', (req, res) => {
+    let htmlPath = path.join(__dirname, './views/login.html');
     res.sendFile (htmlPath)
 })
+
+app.listen(port, ()=> console.log("servidor corriendo en el puerto ${port}"));
 
 /*revert*/
     
